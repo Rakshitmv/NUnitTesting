310ca3c9af8258349281cce4244b67fbf1e48120
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-01 22:29:50 +0530

    [Rakshit] Add: Day of the week program completed

---
+++ NUnitTesting/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter 1: To get Day of the week\n" + "Enter 2: Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        DayOfWeek.Input();
+                        break;
+
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter the correct option\n");
+                        break;
+
+
+                }
+
+            }
+        }
+    }
+}
