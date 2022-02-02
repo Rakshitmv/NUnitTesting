4030784ac4fd20f7d257a2c484d0f3c454616786
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-02 18:14:58 +0530

    [Rakshit] Add: Decimal to Binary program completed

--- NUnitTesting/Program.cs
+++ NUnitTesting/Program.cs
@@ -13,7 +13,7 @@ namespace NUnitTesting
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter 1: To get Day of the week\n" + "Enter 2: Vending Machine\n" +  "Enter 3: Temperature Conversion\n" + "Enter 4: Monthly Payment\n" + "Enter 5: Square Roots\n" + "Enter 6: Exit");
+                Console.WriteLine("Enter 1: To get Day of the week\n" + "Enter 2: Vending Machine\n" +  "Enter 3: Temperature Conversion\n" + "Enter 4: Monthly Payment\n" + "Enter 5: Square Roots\n" + "Enter 6: Get decimal to binary\n" + "Enter 7: Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -37,6 +37,10 @@ namespace NUnitTesting
                         squareRoot.SqrtRoot();
                         break;
                     case 6:
+                        ToBinary toBinary = new ToBinary();
+                        toBinary.Binary();
+                        break;
+                    case 7:
                         flag = false;
                         break;
                     default:
---
+++ NUnitTesting/ToBinary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTesting
+{
+    public class ToBinary
+    {
+        public string ReverseString(string str)
+        {
+            char[] chars = str.ToCharArray();
+            for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+            {
+                char c = chars[i];
+                chars[i] = chars[j];
+                chars[j] = c;
+            }
+            return new string(chars);
+        }
+        public void Binary()
+        {
+            Console.WriteLine("Enter the number\n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            var binary = new StringBuilder();
+            while (n > 0)
+            {
+               
+                int current = n % 2;
+                binary.Append(current);
+                n /= 2;
+            }
+            Console.WriteLine(binary.Length);
+            string temp = binary.ToString();
+            Console.WriteLine(ReverseString(temp));
+
+        }
+    }
+}
